0ea897127721233f8cd2bf1ab6361939b02a98d4
Thomas Gronwall <thomas.gronwall@gmail.com>
2014-10-10 14:13:35 +0200

    Removed implicit dependency to junit

--- bedede/src/main/java/com/github/thogr/bedede/conditions/BooleanConditionVerifier.java
+++ bedede/src/main/java/com/github/thogr/bedede/conditions/BooleanConditionVerifier.java
@@ -1,14 +1,14 @@
 package com.github.thogr.bedede.conditions;
 
-import org.junit.Assert;
-
 import com.github.thogr.bedede.Otherwise;
 
 public class BooleanConditionVerifier implements ConditionVerifier<BooleanCondition> {
 
     @Override
     public Boolean verify(final BooleanCondition condition, final Otherwise otherwise) {
-        Assert.assertTrue(otherwise.getMessage(), condition.value());
+        if (!condition.value()) {
+            throw new AssertionError(otherwise.getMessage());
+        }
         return condition.value();
     }
 
