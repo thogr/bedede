48a421f9c045071d9f825fcf347b65ea13035107
Thomas Gronwall <thomas.gronwall@gmail.com>
2015-06-06 09:34:08 +0200

    Corrected Groovy example

--- bedede-core-examples/src/test/groovy/com/github/thogr/bedede/examples/MixedGroovyExampleTest.groovy
+++ bedede-core-examples/src/test/groovy/com/github/thogr/bedede/examples/MixedGroovyExampleTest.groovy
@@ -4,6 +4,7 @@ import static com.github.thogr.bedede.Bedede.it;
 import static com.github.thogr.bedede.Bedede.performing;
 import static com.github.thogr.bedede.Bedede.transforming;
 import static org.hamcrest.CoreMatchers.is;
+import static com.github.thogr.bedede.Bedede.*
 
 import org.junit.Test;
 
