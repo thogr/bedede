c9f554b1877621cb358a5cc94163e44cab5123c0
Thomas Gronwall <thomas.gronwall@gmail.com>
2015-06-26 18:07:18 +0200

    Added more unit test for AnnotatedStateVerifier

--- bedede-core/src/test/java/com/github/thogr/bedede/AnnotatedStateVerifierTest.java
+++ bedede-core/src/test/java/com/github/thogr/bedede/AnnotatedStateVerifierTest.java
@@ -4,6 +4,9 @@ import static com.github.thogr.bedede.Bedede.expecting;
 import static com.github.thogr.bedede.Bedede.given;
 import static com.github.thogr.bedede.Bedede.otherwise;
 import static com.github.thogr.bedede.Bedede.performing;
+import static com.github.thogr.bedede.Bedede.then;
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
 import static org.mockito.BDDMockito.then;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.never;
@@ -29,6 +32,8 @@ public class AnnotatedStateVerifierTest {
     private State1 state1;
     private State2 state2;
     private State3 state3;
+    private IllegalState1 illegalState1;
+    private Throwable expected = null;
 
     public static class State0 {
 
@@ -55,39 +60,70 @@ public class AnnotatedStateVerifierTest {
 
     }
 
+    public static class IllegalState1 {
+         @OnEntry
+         public void entryMethod() throws IllegalAccessException {
+             throw new IllegalAccessException();
+         }
+    }
 
     @Before
     public void setUp() throws Exception {
         state1 = new State1();
         state2 = new State2();
         state3 = new State3();
+        illegalState1 = new IllegalState1();
     }
 
     @Test
     public void shouldNotVerifyWhenNoEntryMethod() {
-        given(new AnnotatedStateVerifier<State0>(State0.class, conditionController))
+        given(aVerifierFor(State0.class))
         .when(performing(the -> the.verify(state0)));
         then(conditionController).should(never()).verify(any());
     }
 
     @Test
     public void shouldCallSimpleEntryMethod() {
-        given(new AnnotatedStateVerifier<State1>(State1.class, conditionController))
+        given(aVerifierFor(State1.class))
         .when(performing(the -> the.verify(state1)))
         .then(the -> state1.isVerified);
     }
 
     @Test
     public void shouldVerifyConditionFromEntryMethod() {
-        given(new AnnotatedStateVerifier<State2>(State2.class, conditionController))
+        given(aVerifierFor(State2.class))
         .when(performing(the -> the.verify(state2)));
         then(conditionController).should().verify(State2.condition);
     }
 
     @Test
     public void shouldVerifyConditionFromEntryMethodInSuperclass() {
-        given(new AnnotatedStateVerifier<State3>(State3.class, conditionController))
+        given(aVerifierFor(State3.class))
         .when(performing(the -> the.verify(state3)));
         then(conditionController).should().verify(State2.condition);
+    }
+
+    @Test
+    public void shouldFailWhenIllegalAccessExceptionOnEntryMethod() throws Exception {
+        try {
+            given(aVerifierFor(IllegalState1.class))
+            .when(performing(the -> the.verify(illegalState1)));
+        } catch (Exception e) {
+            expected = e;
+        }
+        then(conditionController).should(never()).verify(any());
+        then(cantVerify("IllegalState1"));
+    }
+
+    private <T> AnnotatedStateVerifier<T> aVerifierFor(Class<T> state) {
+        return new AnnotatedStateVerifier<>(state, conditionController);
+    }
+
+    private Behavior<?> cantVerify(String name) {
+        return
+        then(expected, is(instanceOf(IllegalArgumentException.class)))
+        .then(the -> expected.getMessage(),
+                is("Can't verify state com.github.thogr.bedede.AnnotatedStateVerifierTest$"
+                        + name));
     }
 }
