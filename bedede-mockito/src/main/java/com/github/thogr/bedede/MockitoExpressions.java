95c74e605f1537abbe11b21dfecb3e7d1cb258d9
Thomas Gronwall <thomas.gronwall@gmail.com>
2015-09-12 08:35:54 +0200

    * Rename MockExpressionsImpl -> MockitoExpressionsImpl

--- bedede-mockito/src/main/java/com/github/thogr/bedede/MockitoExpressions.java
+++ bedede-mockito/src/main/java/com/github/thogr/bedede/MockitoExpressions.java
@@ -3,14 +3,14 @@ package com.github.thogr.bedede;
 import org.mockito.BDDMockito;
 import org.mockito.BDDMockito.BDDMyOngoingStubbing;
 
-import com.github.thogr.bedede.mockito.internal.MockExpressionsImpl;
+import com.github.thogr.bedede.mockito.internal.MockitoExpressionsImpl;
 import com.github.thogr.bedede.mocks.Mocked;
 import com.github.thogr.bedede.mocks.That;
 
 public final class MockitoExpressions {
 
-    private static MockExpressionsImpl impl =
-            new MockExpressionsImpl(new MockitoExpressions());
+    private static MockitoExpressionsImpl impl =
+            new MockitoExpressionsImpl(new MockitoExpressions());
 
     private MockitoExpressions() {
     }
--- bedede-mockito/src/main/java/com/github/thogr/bedede/mockito/internal/MockitoExpressionsImpl.java
+++ bedede-mockito/src/main/java/com/github/thogr/bedede/mockito/internal/MockitoExpressionsImpl.java
@@ -5,21 +5,25 @@ import org.mockito.BDDMockito.BDDMyOngoingStubbing;
 import org.mockito.BDDMockito.Then;
 
 import com.github.thogr.bedede.MockitoExpressions;
+import com.github.thogr.bedede.core.internal.Internal;
 import com.github.thogr.bedede.mocks.Mocked;
 import com.github.thogr.bedede.mocks.That;
 
-public final class MockExpressionsImpl {
+public final class MockitoExpressionsImpl {
 
-    public MockExpressionsImpl(final MockitoExpressions expressions) {
+    @Internal
+    public MockitoExpressionsImpl(final MockitoExpressions expressions) {
         if (expressions == null) {
             throw new NullPointerException();
         }
     }
 
+    @Internal
     public <T> That<BDDMyOngoingStubbing<T>> that(final T methodCall) {
         return new MockitoThat<>(BDDMockito.given(methodCall));
     }
 
+    @Internal
     public <T> Mocked<Then<T>> theMocked(final T mock) {
         return new MockitoMocked<>(BDDMockito.then(mock));
     }
