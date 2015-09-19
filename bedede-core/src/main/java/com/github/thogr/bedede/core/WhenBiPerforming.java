de4ef867947ae3517837a9893a95159faa270180
Thomas Gronwall <thomas.gronwall@gmail.com>
2015-09-19 14:58:37 +0200

    Remove ContinuedWhen (no needed)

--- bedede-core/src/main/java/com/github/thogr/bedede/core/WhenBiPerforming.java
+++ bedede-core/src/main/java/com/github/thogr/bedede/core/WhenBiPerforming.java
@@ -1,5 +1,5 @@
 package com.github.thogr.bedede.core;
 
-public interface WhenBiPerforming<T1, T2> extends ContinuedWhen<T1, T2> {
+public interface WhenBiPerforming<T1, T2> extends ContinuedBehaviorExpression<T1, T2> {
 
 }
